55912bd5407de8920013d780e22219552d8188d1
sonji406 <wkaqh24@gmail.com>
2024-11-13 00:25:40 +0900

    Chore: totalAmount 더미 데이터 추가

--- src/components/Cart/Footer/index.tsx
+++ src/components/Cart/Footer/index.tsx
@@ -4,7 +4,7 @@ import TotalAmount from './TotalAmount';
 const Footer = () => {
   return (
     <div className='border-solid border-t-2 border-gray-100 px-5'>
-      <TotalAmount />
+      <TotalAmount totalAmount={100000} />
       <NextStepButton />
     </div>
   );
