23b1263b16055b62eb82b01093cf4bef92cc8710
sonji406 <wkaqh24@gmail.com>
2024-11-12 16:42:24 +0900

    Feat: 장바구니 버튼 아래 border 추가, 결제 날짜 수정

--- src/components/Cart/PaymentInformation.tsx
+++ src/components/Cart/PaymentInformation.tsx
@@ -3,13 +3,13 @@ import formatDate from '../../utils/formatDate';
 const PaymentInformation = () => {
   const paymentData = {
     fullName: '곽지우',
-    completedDate: formatDate(new Date(2019, 6, 14, 17, 30)),
+    completedDate: formatDate(new Date()),
   };
 
   return (
     <div className='absolute left-1/2 transform -translate-x-1/2 text-center'>
       <p className='text-lg font-medium'>{paymentData.fullName}</p>
-      <p className='text-sm text-gray-400 font-semibold'>{paymentData.completedDate}</p>
+      <p className='text-sm text-gray-300 font-medium'>{paymentData.completedDate}</p>
     </div>
   );
 };
