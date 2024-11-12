69920ae5253e642c4fe7bda7240d8d3deffb6cc0
sonji406 <wkaqh24@gmail.com>
2024-11-12 16:30:48 +0900

    Feat: 장바구니 시술, 할인 추가 버튼 ui 작업

--- src/components/Cart/ButtonGroup.tsx
+++ src/components/Cart/ButtonGroup.tsx
@@ -1,8 +1,14 @@
 const ButtonGroup = () => {
   return (
-    <div>
-      <button>시술</button>
-      <button>할인</button>
+    <div className='flex gap-x-2 px-1'>
+      <button className='basis-1/2 rounded-xl bg-gray-100 py-3 flex items-center justify-center'>
+        <img src='/images/icon/gray_plus_icon.png' alt='Plus Icon' className='w-4 h-4 mr-1 ' />
+        <span className='text-gray-400 font-semibold align-middle mt-0.5'>시술</span>
+      </button>
+      <button className='basis-1/2 rounded-xl bg-rose-50 py-3 flex items-center justify-center'>
+        <img src='/images/icon/pink_plus_icon.png' alt='Plus Icon' className='w-4 h-4 mr-1 ' />
+        <span className='text-pink-400 font-semibold align-middle mt-0.5'>할인</span>
+      </button>
     </div>
   );
 };
