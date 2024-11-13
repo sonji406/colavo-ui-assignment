033a450f3fccd7dab7cd2ece1f6c98c0b1ad86fb
sonji406 <wkaqh24@gmail.com>
2024-11-14 02:37:48 +0900

    Feat: 화면 전환 버튼 적용

--- src/components/Cart/ButtonGroup/index.tsx
+++ src/components/Cart/ButtonGroup/index.tsx
@@ -2,19 +2,29 @@ import { useViewContext } from 'contexts/ViewContext';
 import Button from './Button';
 
 const ButtonGroup = () => {
-  const { currentView } = useViewContext();
+  const { currentView, changeView } = useViewContext();
+
+  const handleItemMenu = () => {
+    changeView('itemMenu');
+  };
+
+  const handleDiscountMenu = () => {
+    changeView('discountMenu');
+  };
 
   return (
     <>
       {currentView === 'main' && (
         <div className='flex gap-x-2 mx-5 pb-4 border-b-2 border-dashed border-slate-100'>
           <Button
+            onClick={handleItemMenu}
             icon='/images/icon/gray_plus_icon.png'
             bgColor='bg-gray-100'
             textColor='text-gray-400'
             text='시술'
           />
           <Button
+            onClick={handleDiscountMenu}
             icon='/images/icon/pink_plus_icon.png'
             bgColor='bg-rose-50'
             textColor='text-pink-400'
