db98705247bfde91efbb315b4658931141f57c34
sonji406 <wkaqh24@gmail.com>
2024-11-13 22:25:50 +0900

    Chore: 장바구니 리스트 컴포넌트 이름 수정

---
+++ src/components/Cart/CheckoutList/index.tsx
@@ -0,0 +1,49 @@
+import CheckoutDiscounts from './CheckoutDiscounts';
+import CheckoutItems from './CheckoutItems';
+
+const itemData = {
+  i_1: {
+    count: 1,
+    name: '여성컷',
+    price: 35000,
+  },
+  i_2: {
+    count: 1,
+    name: '남성컷',
+    price: 30000,
+  },
+  i_3: {
+    count: 1,
+    name: '드라이',
+    price: 30000,
+  },
+  i_4: {
+    count: 1,
+    name: '기본펌',
+    price: 100000,
+  },
+};
+
+const discountData = {
+  d_1: {
+    name: '지인 할인',
+    rate: 0.08,
+  },
+  d_2: {
+    name: '학생 할인',
+    rate: 0.07,
+  },
+};
+
+const CheckoutList = () => {
+  const totalPrice = Object.values(itemData).reduce((sum, item) => sum + item.price, 0);
+
+  return (
+    <div className='flex-grow px-5'>
+      <CheckoutItems itemData={Object.values(itemData)} />
+      <CheckoutDiscounts totalPrice={totalPrice} discountData={Object.values(discountData)} />
+    </div>
+  );
+};
+
+export default CheckoutList;
